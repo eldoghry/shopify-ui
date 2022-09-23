4e385feb060cc7ded828dbb03884d411ef30156f
mma-90 <moh.mag.ali@gmail.com>
2022-09-23 03:02:45 +0200

    implement Announcement

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 3rem;
+  padding: 1rem 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: teal;
+  color: white;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Free Shipping on orders that exceed 200$</Container>;
+};
+
+export default Announcement;
